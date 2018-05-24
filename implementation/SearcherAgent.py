5f07683834aecc6ce8b945a1e78d711e4e89dda4
Drag0s <me@dragosfotescu.com>
2018-05-24 10:39:26 +0200

    Bootstrap SearcherAgent

---
+++ implementation/SearcherAgent.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 15:58:13 2013
+
+Esqueleto de agente usando los servicios web de Flask
+
+/comm es la entrada para la recepcion de mensajes del agente
+/Stop es la entrada que para el agente
+
+Tiene una funcion AgentBehavior1 que se lanza como un thread concurrente
+
+Asume que el agente de registro esta en el puerto 9000
+
+@author: javier
+"""
+
+from __future__ import print_function
+from multiprocessing import Process, Queue
+import socket
+
+from rdflib import Namespace, Graph
+from flask import Flask, request
+
+from AgentUtil.FlaskServer import shutdown_server
+from AgentUtil.Agent import Agent
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+import os
+import bottlenose
+from bs4 import BeautifulSoup
+
+amazon = bottlenose.Amazon(
+    os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], os.environ['AWS_ASSOCIATE_TAG'], Region='ES',
+    Parser=lambda text: BeautifulSoup(text, 'xml')
+)
+
+__author__ = 'javier'
+
+
+# Configuration stuff
+hostname = socket.gethostname()
+port = 9010
+
+agn = Namespace("http://www.agentes.org#")
+
+# Contador de mensajes
+mss_cnt = 0
+
+# Datos del Agente
+
+AgentePersonal = Agent('AgenteSimple',
+                       agn.AgenteSimple,
+                       'http://%s:%d/comm' % (hostname, port),
+                       'http://%s:%d/Stop' % (hostname, port))
+
+# Directory agent address
+DirectoryAgent = Agent('DirectoryAgent',
+                       agn.Directory,
+                       'http://%s:9000/Register' % hostname,
+                       'http://%s:9000/Stop' % hostname)
+
+
+# Global triplestore graph
+dsgraph = Graph()
+
+cola1 = Queue()
+
+# Flask stuff
+app = Flask(__name__)
+
+
+@app.route("/comm")
+def comunicacion():
+    """
+    Entrypoint de comunicacion
+    """
+    global dsgraph
+    global mss_cnt
+    pass
+
+
+@app.route("/Stop")
+def stop():
+    """
+    Entrypoint que para el agente
+
+    :return:
+    """
+    tidyup()
+    shutdown_server()
+    return "Parando Servidor"
+
+
+
+@app.route("/search")
+def search():
+    """
+    Endpoint that returns search of query.
+    :return: amazon_items
+    """
+
+    query = request.args['query']
+
+    response = None
+    try:
+        response = amazon.ItemSearch(Keywords=query, SearchIndex="All")
+        print(response)
+    except Exception as e: print(e)
+
+    return response.prettify()
+
+
+
+def tidyup():
+    """
+    Acciones previas a parar el agente
+
+    """
+    pass
+
+
+def agentbehavior1(cola):
+    """
+    Un comportamiento del agente
+
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    # Ponemos en marcha los behaviors
+    ab1 = Process(target=agentbehavior1, args=(cola1,))
+    ab1.start()
+
+    # Ponemos en marcha el servidor
+    app.run(host=hostname, port=port)
+
+    # Esperamos a que acaben los behaviors
+    ab1.join()
+    print('The End')
+
+

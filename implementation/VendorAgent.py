979c4608b68c02571b5b97ccbbd154a91b223b2f
gerardEsteve <gerard_Esteve@hotmail.com>
2018-06-11 16:16:50 +0200

    cambios minimos

--- implementation/VendorAgent.py
+++ implementation/VendorAgent.py
@@ -90,6 +90,11 @@ def comunicacion():
             return "devolución denegada por la tienda"
         return "devolución aceptada"
 
+    if action == OntologyConstants.ACTION_ADD_EXT:
+        #añadir producto
+        #los parametros estaran en el graph
+        s = ""
+
     url = "http://" + hostname + ":" + "9011" + "/comm"
 
     print("creem messageDataGo Pedido")
